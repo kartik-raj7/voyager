f3e8105b1ec2a9c799ff4a46c743cf95b27940bd
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-08 16:27:44 +0530

    Fixed video

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,7 +5,7 @@ import { Button } from './Button'
 function MainSection() {
   return (
     <div className='main-container'>
-    <video src ='videos\home.mp4' autoPlay loop muted></video> 
+    <video src ='videos\Home.mp4' autoPlay loop muted></video> 
     <h1>Planning your Dream Travel</h1>
     <p>Taking You Closer to your Holiday Destination</p>
     <div className='main-btns'>
