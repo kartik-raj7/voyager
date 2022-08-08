12f00572588b47ddc3b3c88f36d8d7d96a64c66a
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-08 16:13:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 // import { Routes ,Route } from 'react-router-dom';
 import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/Sign-Up';
 function App() {
   return (
     <>
@@ -11,9 +14,10 @@ function App() {
         <Routes>
           {/* <Route exact path = '/'></Route> */}
           <Route exact path='/' element={<Home/>} />
-          {/* <Route path='/services' element={<Services/>} />
-          <Route path='/products' element={<Products/>} />
-          <Route path='/sign-up' element={<SignUp/>} /> */} 
+          <Route exact path='*' element={<Home/>}/>
+          <Route path='/about' element={<Services/>} />
+          <Route path='/contact-us' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />  
         </Routes>
       </Router>
       </>
